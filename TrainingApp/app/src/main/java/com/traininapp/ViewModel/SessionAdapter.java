1e15036523783849b0bba0bfb485116d3ae7af30
Mathias <lammers@student.chalmers.se>
2019-10-02 17:09:22 +0200

    Working code after help from Alex, but needs fixed

--- TrainingApp/app/src/main/java/com/traininapp/ViewModel/SessionAdapter.java
+++ TrainingApp/app/src/main/java/com/traininapp/ViewModel/SessionAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -11,29 +12,34 @@ import com.traininapp.Model.Session;
 import com.traininapp.R;
 
 import java.util.ArrayList;
+import java.util.List;
 
+// Provide a reference to the views for each data item
 public class SessionAdapter extends RecyclerView.Adapter<SessionHolder> {
 
-    private Context context;
-    private ArrayList<Session> sessions;
+    private List<Session> sessions;
 
-    public SessionAdapter(Context context, ArrayList<Session> sessions) {
-        this.context = context;
+    // Adapter constructor
+    public SessionAdapter(List<Session> sessions) {
         this.sessions = sessions;
     }
 
+    // Create new views (invoked by the layout manager)
     @Override
     public SessionHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(context).inflate(R.layout.session_row, parent, false);
-        return new SessionHolder(view);
+        TextView v = (TextView) LayoutInflater.from(parent.getContext()).inflate(R.layout.session_row, parent, false);
+        SessionHolder vh = new SessionHolder(v);
+        return vh;
     }
 
+    // Replace the contents of a view (invoked by the layout manager)
     @Override
     public void onBindViewHolder(SessionHolder holder, int position) {
-        Session session = sessions.get(position);
-        holder.setDetails(session);
+        //Session session = sessions.get(position);
+        holder.setDetails("bla" + position);
     }
 
+    // Return the size of dataset (invoked by the layout manager)
     @Override
     public int getItemCount() {
         return sessions.size();
